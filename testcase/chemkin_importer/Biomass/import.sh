c1da3ef78f446ab86ecf57aadfb30d69fa5ab6f0
faribas <seyedzadehkhanshan.f@husky.neu.edu>
2013-07-30 17:29:15 -0400

    Added another example chemkin file: Ranzi's biomass pyrolysis

---
+++ testcase/chemkin_importer/Biomass/import.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Run under a profiler
+# Serve on Port 8081
+
+python -m cProfile -o importChemkin.profile $RMGpy/importChemkin.py  --species SpRxn.dat --reactions SpRxn.dat --thermo Thermo.dat  --port 8082
+gprof2dot -f pstats  importChemkin.profile | dot -Tpdf -o importChemkin.profile.pdf
