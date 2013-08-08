90cd6eb16d1beeda2677c6fbdb91f0a3b9b36c62
Fariba Seyedzadeh <seyedzadehkhanshan.f@husky.neu.edu>
2013-08-08 16:25:45 -0400

    Some changes in thermo and species files of biomass example!

--- testcase/chemkin_importer/Biomass/import.sh
+++ testcase/chemkin_importer/Biomass/import.sh
@@ -2,5 +2,5 @@
 # Run under a profiler
 # Serve on Port 8081
 
-python -m cProfile -o importChemkin.profile $RMGpy/importChemkin.py  --species SpRxn.dat --reactions SpRxn.dat --thermo Thermo.dat  --port 8082
+python -m cProfile -o importChemkin.profile $RMGpy/importChemkin.py  --species SpRxn.dat --reactions SpRxn.dat --thermo Thermo.dat --known SMILES.txt  --port 8082
 gprof2dot -f pstats  importChemkin.profile | dot -Tpdf -o importChemkin.profile.pdf
